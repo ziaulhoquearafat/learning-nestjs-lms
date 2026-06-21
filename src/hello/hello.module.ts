ebbed08d11cd4b838277084ece49407597214f54
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-06-21 07:35:38 +0600

    create hello services and controllers

--- src/hello/hello.module.ts
+++ src/hello/hello.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { HelloController } from './hello.controller';
+import { HelloService } from './hello.service';
 
-@Module({})
+@Module({
+  controllers: [HelloController],
+  providers: [HelloService],
+})
 export class HelloModule {}
